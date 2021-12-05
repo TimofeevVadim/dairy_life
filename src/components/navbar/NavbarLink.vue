7d09d57b82d9230fd67971db4fe5bbd534e1c143
TimofeevVadim <vtimofeev223@gmail.com>
2021-12-05 15:07:49 +0300

    Project preparation

---
+++ src/components/navbar/NavbarLink.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="">link</div>
+</template>
+
+<script>
+export default {
+  name: "NavbarLink",
+};
+</script>
+
+<style scoped></style>
---
+++ src/views/pages/DashboardLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <NavbarLink></NavbarLink>
+  </div>
+</template>
+
+<script>
+import NavbarLink from "../../components/navbar/NavbarLink";
+export default {
+  name: "DashboardLayout",
+  components: {
+    NavbarLink,
+  },
+};
+</script>
+
+<style scoped></style>
